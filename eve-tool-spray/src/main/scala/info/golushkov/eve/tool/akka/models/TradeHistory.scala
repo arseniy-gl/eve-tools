911cc17a2a056a82f656fb1006e24fbca0f7cab4
st11236 <st11236@ptc.local>
2017-09-29 11:13:13 +0300

    Clear Order coll in mongo

--- eve-tool-spray/src/main/scala/info/golushkov/eve/tool/akka/models/TradeHistory.scala
+++ eve-tool-spray/src/main/scala/info/golushkov/eve/tool/akka/models/TradeHistory.scala
@@ -2,5 +2,11 @@ package info.golushkov.eve.tool.akka.models
 
 import java.time.LocalDate
 
-case class TradeHistory(average: Float, date:LocalDate, highest: Float, lowest: Float, orderCount: Long, volume: Long)
+case class TradeHistory(
+                         average: Float,
+                         date:LocalDate,
+                         highest: Float,
+                         lowest: Float,
+                         orderCount: Long,
+                         volume: Long)
 
